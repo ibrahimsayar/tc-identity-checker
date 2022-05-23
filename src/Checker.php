45ed57da9908807b5d8a8fe4edfe7303f31747c3
ibrahim.sayar <ibrahim.sayar@naylalabs.com>
2022-05-23 18:20:45 +0300

    Composer package created.
    Wip.

---
+++ src/Checker.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Checker;
+
+class Checker
+{
+	public function TcCheck(int $identityNumber): bool
+	{
+		if (!is_numeric($identityNumber) || $identityNumber[0] == 0) {
+	        return false;
+	    }
+
+	    $firstAllowCharacter = [1, 3, 5, 7, 9];
+	    $secondAllowCharacter = [2, 4, 6, 8];
+	    $thirdAllowCharacter = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	    $firstSumCharacters = 0;
+	    $secondSumCharacters = 0;
+	    $thirdSumCharacters = 0;
+	    for ($l = 0; $l < strlen($identityNumber); $l++) {
+	        if (in_array(($l + 1), $firstAllowCharacter)) {
+	            $firstSumCharacters += $identityNumber[$l];
+	        }
+	        if (in_array(($l + 1), $secondAllowCharacter)) {
+	            $secondSumCharacters += $identityNumber[$l];
+	        }
+	        if (in_array(($l + 1), $thirdAllowCharacter)) {
+	            $thirdSumCharacters += $identityNumber[$l];
+	        }
+	    }
+
+	    $the10thDigitOfThe = (($firstSumCharacters * 7) - $secondSumCharacters) % 10;
+	    $the11thDigitOfThe = ($thirdSumCharacters + $the10thDigitOfThe) % 10;
+
+	    if ($identityNumber[9] != $the10thDigitOfThe && $the10thDigitOfThe != $the11thDigitOfThe) {
+	        return false;
+	    }
+
+	    return true;
+	}
+}
